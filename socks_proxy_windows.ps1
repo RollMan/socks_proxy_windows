8db149a0fbee621812343c9c2d803a07499ad4da
Yohei Shimmyo <qwpmb554@gmail.com>
2020-06-04 15:23:05 +0900

    Update socks_proxy_windows.ps1

--- socks_proxy_windows.ps1
+++ socks_proxy_windows.ps1
@@ -1,1 +1,15 @@
-netsh winhttp set proxy proxy-server="socks=localhost:8888" bypass-list="localhost"
+# netsh winhttp set proxy proxy-server="socks=localhost:8888" bypass-list="localhost"
+
+# Enable proxy
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings" /f /v ProxyEnable /t reg_dword /d 1
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings" /f /v ProxyServer /t reg_sz /d  "socks=localhost:8888"
+
+# Check registories
+reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /s
+
+# Reset proxy
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings" /f /v ProxyEnable /t reg_dword /d 0
+
+###
+# Proxy cannot become enabled unless I launch "Internet Option" window.
+###
